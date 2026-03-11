b9fc3d16c1f0a285816ec0b51177d1e827a57420
hyeonryeol <hunyeol138@gmail.com>
2026-03-11 21:05:26 +0900

    자료구조 흰돌배치

--- 자료구조3월18일 문제/자료구조3월18일 문제/3월 18일 시험문제.cpp
+++ 자료구조3월18일 문제/자료구조3월18일 문제/3월 18일 시험문제.cpp
@@ -1,15 +1,44 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
-int main()
+char board[19][19];//보드 배열
+
+void inboard()//보드 입력함수
 {
 	for (int i = 0; i < 19;++i)
 	{
 		for (int j = 0; j < 19; ++j)
 		{
-			cout << "+" << " ";
+			board[i][j] = '+';
 		}
 		cout << endl;
 	}
+}
+
+void printboard() //보드 출력함수
+{
+	for (int i = 0; i < 19; i++)
+	{
+		for (int j = 0; j < 19; j++)
+		{
+			cout << board[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+
+int main()
+{
+	inboard();
+	printboard();
+	int x, y;
+	cout << "흰 돌을 놓을 곳을 입력하세요.(행, 열):";
+	cin >> x;
+	cin >> y;
+	int a = x - 1; //배열이라 마이너스 해주기
+	int b = y - 1;
+	board[a][b] = 'O';
+	printboard();
 }
