4b8e9475383fbfa8871fcd19357e095d985f8c0d
hyeonryeol <hunyeol138@gmail.com>
2026-03-12 14:37:16 +0900

    Update 3월 18일 시험문제.cpp

--- 자료구조3월18일 문제/자료구조3월18일 문제/3월 18일 시험문제.cpp
+++ 자료구조3월18일 문제/자료구조3월18일 문제/3월 18일 시험문제.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 char board[19][19];//보드 배열
 
-void inboard()//보드 입력함수
+void inboard()//보드 입력함수(기본 바둑판)
 {
 	for (int i = 0; i < 19;++i)
 	{
@@ -31,14 +31,17 @@ void printboard() //보드 출력함수
 
 int main()
 {
-	inboard();
-	printboard();
-	int x, y;
-	cout << "흰 돌을 놓을 곳을 입력하세요.(행, 열):";
-	cin >> x;
-	cin >> y;
-	int a = x - 1; //배열이라 마이너스 해주기
-	int b = y - 1;
-	board[a][b] = 'O';
-	printboard();
+		inboard();
+		printboard();
+	while (1)
+	{
+		cout << "흰 돌을 놓을 곳을 입력하세요.(행, 열):";
+		int x, y;
+		cin >> x;
+		cin >> y;
+		int a = x - 1; //배열이라 마이너스 해주기
+		int b = y - 1;
+		board[a][b] = 'O';
+		printboard();
+	}
 }
