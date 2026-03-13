07dd55debdeb6f6a38e9f9b1a2fb1dc9372d83aa
hyeonryeol <hunyeol138@gmail.com>
2026-03-13 19:59:53 +0900

    6번 추가

--- 자료구조3월18일 문제/자료구조3월18일 문제/3월 18일 시험문제.cpp
+++ 자료구조3월18일 문제/자료구조3월18일 문제/3월 18일 시험문제.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 char board[19][19];//보드 배열
-
+int wstonecount = 0;
+int bstonecount = 0;
 bool turn = false;
+
 void inboard()//보드 입력함수(기본 바둑판)
 {
 	for (int i = 0; i < 19;++i)
@@ -28,6 +30,7 @@ void printboard() //보드 출력함수
 		}
 		cout << endl;
 	}
+	
 }
 void position(int garo, int sero, char stone) //돌 위치 저장
 {
@@ -51,18 +54,28 @@ int main()
 			int a = x - 1; //배열이라 마이너스 해주기
 			int b = y - 1;
 			
+			
+
 			if (board[b][a] != '+')
 			{
 				cout << "이미 돌이 놓여있습니다. 다른 곳을 입력하세요.";
 			}
+			
+
 			else
 			{
 				position(b, a, 'O');
 				printboard();
+				if (board[b][a] == 'O')
+				{
+					wstonecount++;
+				}
+				cout <<"흰 돌의 갯수:" << wstonecount;
+				cout << endl;
 				turn = true;
 			}
-			
-			
+
+
 		}
 		if (turn == true)
 		{
@@ -81,6 +94,12 @@ int main()
 			{
 				position(b, a, 'X');
 				printboard();
+				if (board[b][a] == 'X')
+				{
+					bstonecount++;
+				}
+				cout << "검은 돌의 갯수:" << bstonecount;
+				cout << endl;
 				turn = false;
 			}
 		}
