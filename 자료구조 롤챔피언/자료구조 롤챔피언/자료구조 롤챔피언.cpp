4126d7410879fef6a35bbffa1afec95c1ef7b3d6
hyeonryeol <hunyeol138@gmail.com>
2026-03-15 16:34:03 +0900

    딜리트 추가

--- 자료구조 롤챔피언/자료구조 롤챔피언/자료구조 롤챔피언.cpp
+++ 자료구조 롤챔피언/자료구조 롤챔피언/자료구조 롤챔피언.cpp
@@ -49,28 +49,85 @@ int main()
 			<< LOL[i].positon << endl;
 	}
 
-	cout << "command를 입력하시오.:" << endl;
-	int command;
-	cin >> command;
-	switch (command)
-	{
-	case 1:
-	{
-		string champ;
-		cin >> champ;
-		for (int i = 0; i < count; ++i)
-		{
-			if (LOL[i].name == champ)
-			{
-				cout << LOL[i].name << " "
-					<< LOL[i].hp << " "
-					<< LOL[i].mp << " "
-					<< LOL[i].speed << " "
-					<< LOL[i].range << " "
-					<< LOL[i].positon << endl;
-			}
 
+	while (1)
+	{
+		cout << "숫자를 입력하시오.(1.search 2.Insert 3.delete ):" << endl;
+		int command;
+		cin >> command;
+		switch (command)
+		{
+		case 1:
+		{
+			bool found = false;
+			string champ;
+			cin >> champ;
+			for (int i = 0; i < count; ++i)
+			{
+				if (LOL[i].name == champ)
+				{
+					cout << LOL[i].name << " "
+						<< LOL[i].hp << " "
+						<< LOL[i].mp << " "
+						<< LOL[i].speed << " "
+						<< LOL[i].range << " "
+						<< LOL[i].positon << endl;
+					found = true;
+					break;
+				}
+			}
+			if (!found)
+			{
+				cout << "그런 챔피언은 존재하지않습니다." << endl;
+			}
+			break;
+		}
+		case 2:
+		{
+			cout << "새로운 챔피언의 이름을 입력하세요." << endl;
+			cin >> LOL[count].name;
+			cout << "새로운 챔피언의 체력을 입력하세요." << endl;
+			cin >> LOL[count].hp;
+			cout << "새로운 챔피언의 마나를 입력하세요." << endl;
+			cin >> LOL[count].mp;
+			cout << "새로운 챔피언의 이동속도를 입력하세요." << endl;
+			cin >> LOL[count].speed;
+			cout << "새로운 챔피언의 사거리를 입력하세요." << endl;
+			cin >> LOL[count].range;
+			cout << "새로운 챔피언의 포지션을 입력하세요." << endl;
+			cin >> LOL[count].positon;
+			count++;
+			break;
+		}
+		case 3:
+		{
+			string champ;
+			cout << "삭제할 챔피언 이름을 입력하세요." << endl;
+			cin >> champ;
+
+			int index = -1;
+
+			for (int i = 0; i < count;++i)
+			{
+				if (LOL[i].name == champ)
+				{
+					index = i;
+					break;
+				}
+			}
+			if (index == -1)
+			{
+				cout << "그런 챔피언은 없습니다."<<endl;
+				break;
+			}
+			for (int i = index; i < count - 1; ++i)
+			{
+				LOL[i] = LOL[i + 1];
+			}
+			count--;
+			break;
+		}
 		}
 	}
-	}
+
 }
