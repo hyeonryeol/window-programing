79bd21067731d7200aff307aeb624a596a764992
hyeonryeol <hunyeol138@gmail.com>
2026-03-16 16:45:51 +0900

    윈플 실습1

---
+++ 윈플 실습1/윈플 실습1/실습3.cpp
@@ -0,0 +1,65 @@
+#include <windows.h> //윈도우 좌표값, 창크기, 색, 크기 조정이 가능한 두꺼운 경계선, 이름
+#include <tchar.h>
+
+HINSTANCE g_hlnst;
+LPCTSTR IpszClass = L"Window Class Name";
+LPCTSTR IpszWindowName = L"windows program 3";
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessgae, WPARAM wParam, LPARAM IParam);
+
+int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevlnstance, LPSTR IpszCmdParam, int nCmdShow)
+{
+	HWND hWND;
+	MSG Message;
+	WNDCLASSEX WndClass;
+	g_hlnst = hlnstance;
+
+	WndClass.cbSize = sizeof(WndClass);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hlnstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = IpszClass;
+	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	RegisterClassEx(&WndClass);
+
+	hWND = CreateWindow(IpszClass, IpszWindowName, WS_OVERLAPPEDWINDOW | WS_SYSMENU | WS_HSCROLL | WS_VSCROLL | WS_MAXIMIZEBOX / WS_MINIMIZEBOX| WS_THICKFRAME, 50, 150, 1000, 600, NULL, (HMENU)NULL, hlnstance, NULL);
+
+	ShowWindow(hWND, nCmdShow);
+	UpdateWindow(hWND);
+
+	while (GetMessage(&Message, 0, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+
+	}
+	return Message.wParam;
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM IParam)
+{
+	PAINTSTRUCT ps;
+	HDC hDC;
+	TCHAR temp[] = TEXT("Hello world");
+	int x = 0, y = 0;
+
+	switch (iMessage) {
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+		TextOut(hDC, x, y, temp, lstrlen(temp));
+		EndPaint(hWnd, &ps);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	}
+	return (DefWindowProc(hWnd, iMessage, wParam, IParam));
+
+}
