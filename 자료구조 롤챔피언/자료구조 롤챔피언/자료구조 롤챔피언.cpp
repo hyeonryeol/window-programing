85aa9f733d410322edbd66b702833fd690a892b3
hyeonryeol <hunyeol138@gmail.com>
2026-03-20 16:06:02 +0900

    Update 자료구조 롤챔피언.cpp

--- 자료구조 롤챔피언/자료구조 롤챔피언/자료구조 롤챔피언.cpp
+++ 자료구조 롤챔피언/자료구조 롤챔피언/자료구조 롤챔피언.cpp
@@ -52,13 +52,18 @@ int main()
 
 	while (1)
 	{
-		cout << "숫자를 입력하시오.(1.search 2.Insert 3.delete ):" << endl;
-		int command;
+		cout << "Search [챔피언이름] : 주어진 챔피언의 정보를 출력한다."<< endl
+			<< "Insert : 새로운 챔피언의 정보를 입력받아 삽입한다." << endl
+			<< "Delete[챔피언이름] : 주어진 챔피언에 대한 정보를 삭제한다." << endl
+			<< "DeleteAll[position] : 주어진 위치의 모든 챔피언 정보를 삭제한다." << endl
+			<< "PrintAll : 모든 챔피언의 정보를 배열에 저장된 순서대로 출력한다." << endl
+			<< "FindMaxHp : 가장 체력이 큰 챔피언의 정보를 출력한다." << endl
+		    << "SortByHp : 체력이 큰 챔피언부터 순서대로 저장한다 : " << endl;
+		string command;
 		cin >> command;
-		switch (command)
+		if (command == "Search")
 		{
-		case 1:
-		{
+
 			bool found = false;
 			string champ;
 			cin >> champ;
@@ -72,17 +77,18 @@ int main()
 						<< LOL[i].speed << " "
 						<< LOL[i].range << " "
 						<< LOL[i].positon << endl;
+
 					found = true;
-					break;
+					
 				}
 			}
 			if (!found)
 			{
 				cout << "그런 챔피언은 존재하지않습니다." << endl;
 			}
-			break;
+			
 		}
-		case 2:
+		else if (command == "Insert")
 		{
 			cout << "새로운 챔피언의 이름을 입력하세요." << endl;
 			cin >> LOL[count].name;
@@ -97,9 +103,9 @@ int main()
 			cout << "새로운 챔피언의 포지션을 입력하세요." << endl;
 			cin >> LOL[count].positon;
 			count++;
-			break;
+			
 		}
-		case 3:
+		else if (command == "Delete")
 		{
 			string champ;
 			cout << "삭제할 챔피언 이름을 입력하세요." << endl;
@@ -107,27 +113,62 @@ int main()
 
 			int index = -1;
 
-			for (int i = 0; i < count;++i)
+			for (int i = 0; i < count; ++i)
 			{
 				if (LOL[i].name == champ)
 				{
 					index = i;
-					break;
+					cout << "삭제되었습니다"<<endl;
 				}
 			}
 			if (index == -1)
 			{
-				cout << "그런 챔피언은 없습니다."<<endl;
-				break;
+				cout << "그런 챔피언은 없습니다." << endl;
+				
 			}
 			for (int i = index; i < count - 1; ++i)
 			{
 				LOL[i] = LOL[i + 1];
 			}
 			count--;
-			break;
+			
 		}
+		else if (command == "DeleteAll")
+		{
+			string pos;
+			cin >> pos;
+
+			int index = -1;
+			for (int i = 0; i < count; ++i)
+			{
+				if (LOL[i].positon == pos)
+				{
+					for (int j = i; j < count - 1; ++j)
+					{
+
+						{
+							LOL[j] = LOL[j + 1];
+						}
+						count--;
+					}
+				}
+				
+			}
+			cout << "삭제되었습니다" << endl;
+		}
+		else if (command == "PrintAll")
+		{
+			for (int i = 0; i < count; ++i)
+			{
+				cout << LOL[i].name << " "
+					<< LOL[i].hp << " "
+					<< LOL[i].mp << " "
+					<< LOL[i].speed << " "
+					<< LOL[i].range << " "
+					<< LOL[i].positon << endl << endl;
+			}
 		}
 	}
-
 }
+
+
