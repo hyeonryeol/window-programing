46365783ba58ee86858054cf585db976e57b4e87
hyeonryeol <hunyeol138@gmail.com>
2026-04-13 00:07:37 +0900

    열심히 공부함

--- 윈플 연습장/윈플 연습장.cpp
+++ 윈플 연습장/윈플 연습장.cpp
@@ -13,8 +13,19 @@ int B = rand() % 255;
 HBRUSH boardbrush;
 HBRUSH wonbrush[10];
 HPEN wonpen = nullptr;
-int g_wonlist[10];
+bool won1 = false;
+bool won2 = false;
+bool won3 = false;
+bool won4 = false;
+bool won5 = false;
+bool won6 = false;
+bool won7 = false;
+bool won8 = false;
+bool won9 = false;
+bool won10 = false;
 
+int g_wonlist[10];
+int g_selected = -1;
 int g_rect[900][4];
 int g_won[900][4];
 void makerectangle()
@@ -139,26 +150,231 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				wonpress++;
 				InvalidateRect(hWnd, nullptr, TRUE);
 			}
-			
+
 		}
 		if (wParam == '1')
 		{
-			g_wonlist[0];
-			wonpen = CreatePen(PS_SOLID, 2, (RGB(255, 0, 0)));
+			if (won1 == false)
+			{
+				g_wonlist[0];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 0;
+				won1 = true;
+			}
+			else
+			{
+				won1 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
 
+
+		}
+		if (wParam == '2')
+		{
+			if (won2 == false)
+			{
+				g_wonlist[1];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 1;
+				won2 = true;
+
+			}
+			else
+			{
+				won2 = false;
+				g_selected = -1;
+			}
 			InvalidateRect(hWnd, nullptr, TRUE);
 		}
-		if(wParam == )
+		if (wParam == '3')
+		{
+			if (won3 == false)
+			{
+				g_wonlist[2];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 2;
+				won3 = true;
+
+			}
+			else
+			{
+				won3 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+		if (wParam == '4')
+		{
+			if (won4 == false)
+			{
+				g_wonlist[3];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 3;
+				won4 = true;
+
+			}
+			else
+			{
+				won4 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+		if (wParam == '5')
+		{
+			if (won5 == false)
+			{
+				g_wonlist[4];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 4;
+				won5 = true;
+
+			}
+			else
+			{
+				won5 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+		if (wParam == '6')
+		{
+			if (won6 == false)
+			{
+				g_wonlist[5];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 5;
+				won6 = true;
+
+			}
+			else
+			{
+				won6 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+		if (wParam == '7')
+		{
+			if (won7 == false)
+			{
+				g_wonlist[6];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 6;
+				won7 = true;
+
+			}
+			else
+			{
+				won7 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+		if (wParam == '8')
+		{
+			if (won8 == false)
+			{
+				g_wonlist[7];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 7;
+				won8 = true;
+
+			}
+			else
+			{
+				won8 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+		if (wParam == '9')
+		{
+			if (won9 == false)
+			{
+				g_wonlist[8];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 8;
+				won9 = true;
+
+			}
+			else
+			{
+				won9 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+		if (wParam == '0')
+		{
+			if (won10 == false)
+			{
+				g_wonlist[9];
+				wonpen = CreatePen(PS_SOLID, 3, (RGB(255, 0, 0)));
+				g_selected = 9;
+				won10 = true;
+
+			}
+			else
+			{
+				won10 = false;
+				g_selected = -1;
+			}
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+
+		if (wParam == VK_RIGHT && (won1 == true || won2 == true || won3 == true || won4 == true || won5 == true || won6 == true || won7 == true || won8 == true || won9 == true || won10 == (true)))
+		{
+			if (g_selected == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+			{
+				int idx = g_wonlist[g_selected];
+				g_won[idx][0] += 30;
+				g_won[idx][2] += 30;
+				InvalidateRect(hWnd, nullptr, TRUE);
+			}
+
+		}
+		if (wParam == VK_LEFT && (won1 == true || won2 == true || won3 == true || won4 == true || won5 == true || won6 == true || won7 == true || won8 == true || won9 == true || won10 == (true)))
+		{
+			if (g_selected == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+			{
+				int idx = g_wonlist[g_selected];
+				g_won[idx][0] -= 30;
+				g_won[idx][2] -= 30;
+				InvalidateRect(hWnd, nullptr, TRUE);
+			}
+		}
+		if (wParam == VK_UP && (won1 == true || won2 == true || won3 == true || won4 == true || won5 == true || won6 == true || won7 == true || won8 == true || won9 == true || won10 == (true)))
+		{
+			if (g_selected == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+			{
+				int idx = g_wonlist[g_selected];
+				g_won[idx][1] -= 30;
+				g_won[idx][3] -= 30;
+				InvalidateRect(hWnd, nullptr, TRUE);
+			}
+		}
+		if (wParam == VK_DOWN && (won1 == true || won2 == true || won3 == true || won4 == true || won5 == true || won6 == true || won7 == true || won8 == true || won9 == true || won10 == (true)))
+		{
+			if (g_selected == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+			{
+				int idx = g_wonlist[g_selected];
+				g_won[idx][1] += 30;
+				g_won[idx][3] += 30;
+				InvalidateRect(hWnd, nullptr, TRUE);
+			}
+		}
 		return 0;
 	}
 	case WM_CREATE:
 	{
 		srand((unsigned int)time(nullptr));  // 시드 설정 (프로그램 시작 시 1번만!)
 		boardbrush = CreateSolidBrush(RGB(255, 255, 255));
-		
-		
+
+
 		makerectangle();
-		
+
 		return 0;
 	}
 	case WM_PAINT:
@@ -172,30 +388,101 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			Rectangle(hdc, g_rect[i][0], g_rect[i][1], g_rect[i][2], g_rect[i][3]);
 		}
 		SelectObject(hdc, oldbrush);
-		
-		
-		
+
+
+
 		for (int i = 0; i < wonpress; ++i)
 		{
-			
+
 			int idx = g_wonlist[i];
 			SelectObject(hdc, wonbrush[i]);
 
 
-			
+
 			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
 		}
 		SelectObject(hdc, oldbrush);
 
-		if (wonpen != nullptr)
+		if (won1 == true)
 		{
-		int idx = g_wonlist[0];
-		HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
-		SelectObject(hdc, wonbrush[0]);
-		Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
-		SelectObject(hdc, oldpen);
+			int idx = g_wonlist[0];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[0]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
 		}
-
+		if (won2 == true)
+		{
+			int idx = g_wonlist[1];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[1]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
+		if (won3 == true)
+		{
+			int idx = g_wonlist[2];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[2]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
+		if (won4 == true)
+		{
+			int idx = g_wonlist[3];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[3]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
+		if (won5 == true)
+		{
+			int idx = g_wonlist[4];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[4]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
+		if (won6 == true)
+		{
+			int idx = g_wonlist[5];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[5]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
+		if (won7 == true)
+		{
+			int idx = g_wonlist[6];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[6]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
+		if (won8 == true)
+		{
+			int idx = g_wonlist[7];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[7]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
+		if (won9 == true)
+		{
+			int idx = g_wonlist[8];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[8]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
+		if (won10 == true)
+		{
+			int idx = g_wonlist[9];
+			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+			SelectObject(hdc, wonbrush[9]);
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+			SelectObject(hdc, oldpen);
+		}
 		EndPaint(hWnd, &ps);
 		return 0;
 	}
