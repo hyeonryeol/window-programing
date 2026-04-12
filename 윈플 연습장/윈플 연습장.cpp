b259e903c9501fd45926e06b19cb62335e772def
hyeonryeol <hunyeol138@gmail.com>
2026-04-12 21:43:06 +0900

    Update 윈플 연습장.cpp

--- 윈플 연습장/윈플 연습장.cpp
+++ 윈플 연습장/윈플 연습장.cpp
@@ -1,28 +1,37 @@
 ﻿#include <windows.h>
-
+#include <cstdlib>   // srand(), rand()
+#include <ctime>     // time()
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 const wchar_t CLASS_NAME[] = L"MyWindowClass";
 const wchar_t WINDOW_TITLE[] = L"Win32 Basic Template";
+int wonpress = 0;
+int R = rand() % 255;
+int G = rand() % 255;
+int B = rand() % 255;
+HBRUSH boardbrush;
+HBRUSH wonbrush[10];
+HPEN wonpen = nullptr;
+int g_wonlist[10];
 
-int g_rect[900][3];
-
-void rectangle()
+int g_rect[900][4];
+int g_won[900][4];
+void makerectangle()
 {
 	int w = 30;
 	int h = 30;
 	for (int i = 0; i < 30; ++i)
 	{
-		for (int j = 30; j < 30; ++j)
+		for (int j = 0; j < 30; ++j)
 		{
-			int count = 0;
+			int count = i * 30 + j;
 
-			int left =j * 30;
-			int top = i * 30;
-			int right = w + left;
-			int bottom = h + top;
-			count++;
+			int left = i * w + 30;
+			int top = j * h + 30;
+			int right = left + w;
+			int bottom = top + h;
+
 			g_rect[count][0] = left;
 			g_rect[count][1] = top;
 			g_rect[count][2] = right;
@@ -30,6 +39,37 @@ void rectangle()
 		}
 	}
 }
+int makewon()
+{
+	int w = 30;
+	int h = 30;
+
+	int i = rand() % 30;
+	int j = rand() % 30;
+
+	int count = i * 30 + j;
+
+	int left = i * w + 30;
+	int top = j * h + 30;
+	int right = left + w;
+	int bottom = top + h;
+
+	g_won[count][0] = left;
+	g_won[count][1] = top;
+	g_won[count][2] = right;
+	g_won[count][3] = bottom;
+
+	return count;
+}
+
+struct won
+{
+	int wonwon[10];
+	int R = rand() % 255;
+	int G = rand() % 255;
+	int B = rand() % 255;
+
+};
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
@@ -75,6 +115,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 }
 
 
+won wonstruct[10];
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
@@ -89,23 +130,70 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	}
 	case WM_KEYDOWN:
 	{
+		if (wParam == 'E')
+		{
+			if (wonpress < 10)
+			{
+				g_wonlist[wonpress] = makewon();  // 어느 칸인지 저장
+				wonbrush[wonpress] = CreateSolidBrush(RGB(wonstruct[wonpress].R, wonstruct[wonpress].G, wonstruct[wonpress].B));
+				wonpress++;
+				InvalidateRect(hWnd, nullptr, TRUE);
+			}
+			
+		}
+		if (wParam == '1')
+		{
+			g_wonlist[0];
+			wonpen = CreatePen(PS_SOLID, 2, (RGB(255, 0, 0)));
 
+			InvalidateRect(hWnd, nullptr, TRUE);
+		}
+		if(wParam == )
 		return 0;
 	}
 	case WM_CREATE:
 	{
+		srand((unsigned int)time(nullptr));  // 시드 설정 (프로그램 시작 시 1번만!)
+		boardbrush = CreateSolidBrush(RGB(255, 255, 255));
 		
-
+		
+		makerectangle();
+		
 		return 0;
 	}
 	case WM_PAINT:
 	{
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
+
+		HBRUSH oldbrush = (HBRUSH)SelectObject(hdc, boardbrush);
 		for (int i = 0; i < 900; ++i)
 		{
 			Rectangle(hdc, g_rect[i][0], g_rect[i][1], g_rect[i][2], g_rect[i][3]);
+		}
+		SelectObject(hdc, oldbrush);
+		
+		
+		
+		for (int i = 0; i < wonpress; ++i)
+		{
+			
+			int idx = g_wonlist[i];
+			SelectObject(hdc, wonbrush[i]);
 
+
+			
+			Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+		}
+		SelectObject(hdc, oldbrush);
+
+		if (wonpen != nullptr)
+		{
+		int idx = g_wonlist[0];
+		HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
+		SelectObject(hdc, wonbrush[0]);
+		Ellipse(hdc, g_won[idx][0], g_won[idx][1], g_won[idx][2], g_won[idx][3]);
+		SelectObject(hdc, oldpen);
 		}
 
 		EndPaint(hWnd, &ps);
@@ -113,7 +201,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	}
 
 	case WM_DESTROY:
-	
+		DeleteObject(wonpen);
+		DeleteObject(wonbrush);
+		DeleteObject(boardbrush);
 		PostQuitMessage(0);
 		return 0;
 	}
