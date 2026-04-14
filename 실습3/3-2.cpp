6c0c2262c9beb9b22e97326025e95cd9e622e33c
hyeonryeol <hunyeol138@gmail.com>
2026-04-14 22:36:35 +0900

    3-2 원 공전구현, 사각형 공전 구현해야함

--- 실습3/3-2.cpp
+++ 실습3/3-2.cpp
@@ -1,7 +1,7 @@
 #include <windows.h>
 #include <cstdlib>
 #include <ctime>
-
+#include <math.h>
 // 윈도우 프로시저 선언
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -11,6 +11,12 @@ const wchar_t WINDOW_TITLE[] = L"Win32 Basic Template";
 HBRUSH wonbrush;
 HPEN wonpen;
 HPEN selectpen;
+HBRUSH gongjeon1;
+HBRUSH gongjeon2;
+HBRUSH gongjeon3;
+HBRUSH gongjeon4;
+
+
 bool won = true;
 bool rect = false;
 bool tri = false;
@@ -19,6 +25,16 @@ bool select2 = false;
 bool select3 = false;
 bool select4 = false;
 
+int cpress1 = 1;
+int cpress2 = 1;
+int cpress3 = 1;
+int cpress4 = 1;
+
+double angle1 = 0.0; // 현재 각도 (라디안)
+double angle2 = 0.0; // 현재 각도 (라디안)
+double angle3 = 0.0; // 현재 각도 (라디안)
+double angle4 = 0.0; // 현재 각도 (라디안)
+int orbitR = 100;   // 궤도 반지름
 // 프로그램 시작점
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
@@ -62,7 +78,89 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 
 	return (int)msg.wParam;
 }
-
+void CALLBACK TimerProc1(HWND hWnd, UINT iMsg, UINT idEvent, DWORD dwTime) //--- 1번 아이디 타이머 함수
+{
+	HDC hdc;
+	if (won == true)
+	{
+		if (cpress1 == 1)
+		{
+			angle1 += 0.1; // 각도 증가 (값 키우면 더 빠름)
+			if (angle1 > 6.28) angle1 = 0.0; // 2π 넘으면 초기화
+		}
+		if (cpress1 == 0)
+		{
+			angle1 -= 0.1; // 각도 증가 (값 키우면 더 빠름)
+			if (angle1 < -6.28) angle1 = 0.0; // 2π 넘으면 초기화
+		}
+	}
+	if (rect == true)
+	{
+		if (cpress1 == 1)
+		{
+			
+		}
+		if (cpress1 == 0)
+		{
+			
+		}
+	}
+	InvalidateRect(hWnd, NULL, TRUE);
+}
+void CALLBACK TimerProc2(HWND hWnd, UINT iMsg, UINT idEvent, DWORD dwTime) //--- 2번 아이디 타이머 함수
+{
+	HDC hdc;
+	if (won == true)
+	{
+		if (cpress2 == 1)
+		{
+			angle2 += 0.2; // 각도 증가 (값 키우면 더 빠름)
+			if (angle2 > 6.28) angle2 = 0.0; // 2π 넘으면 초기화
+		}
+		if (cpress2 == 0)
+		{
+			angle2 -= 0.2; // 각도 증가 (값 키우면 더 빠름)
+			if (angle2 < -6.28) angle2 = 0.0; // 2π 넘으면 초기화
+		}
+	}
+	InvalidateRect(hWnd, NULL, TRUE);
+}
+void CALLBACK TimerProc3(HWND hWnd, UINT iMsg, UINT idEvent, DWORD dwTime) //--- 3번 아이디 타이머 함수
+{
+	HDC hdc;
+	if (won == true)
+	{
+		if (cpress3 == 1)
+		{
+			angle3 += 0.3; // 각도 증가 (값 키우면 더 빠름)
+			if (angle3 > 6.28) angle3 = 0.0; // 2π 넘으면 초기화
+		}
+		if (cpress3 == 0)
+		{
+			angle3 -= 0.3; // 각도 증가 (값 키우면 더 빠름)
+			if (angle3 < -6.28) angle3 = 0.0; // 2π 넘으면 초기화
+		}
+	}
+	InvalidateRect(hWnd, NULL, TRUE);
+}
+void CALLBACK TimerProc4(HWND hWnd, UINT iMsg, UINT idEvent, DWORD dwTime) //--- 4번 아이디 타이머 함수
+{
+	HDC hdc;
+	if (won == true)
+	{
+		if (cpress4 == 1)
+		{
+			angle4 += 0.4; // 각도 증가 (값 키우면 더 빠름)
+			if (angle4 > 6.28) angle4 = 0.0; // 2π 넘으면 초기화
+		}
+		if (cpress4 == 0)
+		{
+			angle4 -= 0.4; // 각도 증가 (값 키우면 더 빠름)
+			if (angle4 < -6.28) angle4 = 0.0; // 2π 넘으면 초기화
+		}
+	}
+	InvalidateRect(hWnd, NULL, TRUE);
+}
 // 윈도우 메시지 처리 함수
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -98,8 +196,85 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		}
 		if (wParam == '1')
 		{
+
 			select1 = true;
+			select2 = false;
+			select3 = false;
+			select4 = false;
 			InvalidateRect(hWnd, NULL, TRUE);
+		}
+		if (wParam == '2')
+		{
+			select1 = false;
+			select2 = true;
+			select3 = false;
+			select4 = false;
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
+		if (wParam == '3')
+		{
+			select1 = false;
+			select2 = false;
+
+			select3 = true;
+			select4 = false;
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
+		if (wParam == '4')
+		{
+			select1 = false;
+			select2 = false;
+			select3 = false;
+			select4 = true;
+
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
+		if (wParam == 'C')
+		{
+			if (select1 == true)
+			{
+				if (cpress1 == 0)
+				{
+					cpress1 = 1;
+				}
+				else if (cpress1 == 1)
+				{
+					cpress1 = 0;
+				}
+			}
+			if (select2 == true)
+			{
+				if (cpress2 == 0)
+				{
+					cpress2 = 1;
+				}
+				else if (cpress2 == 1)
+				{
+					cpress2 = 0;
+				}
+			}
+			if (select3 == true)
+			{
+				if (cpress3 == 0)
+				{
+					cpress3 = 1;
+				}
+				else if (cpress3 == 1)
+				{
+					cpress3 = 0;
+				}
+			}
+			if (select4 == true)
+			{
+				if (cpress4 == 0)
+				{
+					cpress4 = 1;
+				}
+				else if (cpress4 == 1)
+				{
+					cpress4 = 0;
+				}
+			}
 		}
 		return 0;
 	}
@@ -108,6 +283,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		wonpen = CreatePen(PS_SOLID, 3, RGB(0, 200, 255));
 		wonbrush = CreateSolidBrush(RGB(r, g, b));
 		selectpen = CreatePen(PS_SOLID, 3, RGB(0, 0, 255));
+		gongjeon1 = CreateSolidBrush(RGB(0, 200, 255));
+		gongjeon2 = CreateSolidBrush(RGB(0, 0, 255));
+		gongjeon3 = CreateSolidBrush(RGB(255, 200, 255));
+		gongjeon4 = CreateSolidBrush(RGB(0, 200, 20));
+		SetTimer(hWnd, 1, 500, (TIMERPROC)TimerProc1);
+		SetTimer(hWnd, 2, 500, (TIMERPROC)TimerProc2);
+		SetTimer(hWnd, 3, 500, (TIMERPROC)TimerProc3);
+		SetTimer(hWnd, 4, 500, (TIMERPROC)TimerProc4);
 		return 0;
 	}
 
@@ -122,6 +305,32 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		Ellipse(hdc, x - r, 3 * y - r, x + r, 3 * y + r);
 		Ellipse(hdc, 3 * x - r, y - r, 3 * x + r, y + r);
 		Ellipse(hdc, 3 * x - r, 3 * y - r, 3 * x + r, 3 * y + r);
+		SelectObject(hdc, oldbrush);
+
+		int gr = 10;
+		int ox1 = (int)(orbitR * cos(angle1)); // x 오프셋
+		int oy1 = (int)(orbitR * sin(angle1)); // y 오프셋
+
+		int ox2 = (int)(orbitR * cos(angle2)); // x 오프셋
+		int oy2 = (int)(orbitR * sin(angle2)); // y 오프셋
+
+		int ox3 = (int)(orbitR * cos(angle3)); // x 오프셋
+		int oy3 = (int)(orbitR * sin(angle3)); // y 오프셋
+
+		int ox4 = (int)(orbitR * cos(angle4)); // x 오프셋
+		int oy4 = (int)(orbitR * sin(angle4)); // y 오프셋
+		oldbrush = (HBRUSH)SelectObject(hdc, gongjeon1);
+		Ellipse(hdc, x - gr + ox1, y - gr + oy1, x + gr + ox1, y + gr + oy1);
+		SelectObject(hdc, oldbrush);
+		oldbrush = (HBRUSH)SelectObject(hdc, gongjeon2);
+		Ellipse(hdc, x - gr + ox2, 3 * y - gr + oy2, x + gr + ox2, 3 * y + gr + oy2);
+		SelectObject(hdc, oldbrush);
+		oldbrush = (HBRUSH)SelectObject(hdc, gongjeon3);
+		Ellipse(hdc, 3 * x - gr + ox3, y - gr + oy3, 3 * x + gr + ox3, y + gr + oy3);
+		SelectObject(hdc, oldbrush);
+		oldbrush = (HBRUSH)SelectObject(hdc, gongjeon4);
+		Ellipse(hdc, 3 * x - gr + ox4, 3 * y - gr + oy4, 3 * x + gr + ox4, 3 * y + gr + oy4);
+		SelectObject(hdc, oldbrush);
 		SelectObject(hdc, oldbrush);
 		if (won == true)
 		{
@@ -154,7 +363,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			SelectObject(hdc, nullbrush);
 			HPEN oldpen = (HPEN)SelectObject(hdc, wonpen);
 			int r1 = 100;
-			POINT p1[3] = { { r1+r1, y - r1 }, {x - r1 , y + r1}, {x + r1,  y + r1} };
+			POINT p1[3] = { { r1 + r1, y - r1 }, {x - r1 , y + r1}, {x + r1,  y + r1} };
 			Polygon(hdc, p1, 3);
 			POINT p2[3] = { { r1 + r1, 3 * y - r1 }, {x - r1 , 3 * y + r1}, {x + r1, 3 * y + r1} };
 			Polygon(hdc, p2, 3);
@@ -162,7 +371,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			Polygon(hdc, p3, 3);
 			POINT p4[4] = { { 3 * (r1 + r1) - 10,3 * y - r1 }, {3 * x - r1 , 3 * y + r1}, {3 * x + r1, 3 * y + r1} };
 			Polygon(hdc, p4, 3);
-			
+
 			SelectObject(hdc, oldpen);
 		}
 		HBRUSH nullbrush = (HBRUSH)GetStockObject(NULL_BRUSH); //채우기 없는 브러쉬
@@ -171,10 +380,25 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		int r1 = 120;
 		if (select1 == true)
 		{
-		Rectangle(hdc, x - r1, y - r1, x + r1, y + r1);
-		SelectObject(hdc, oldpen);
+			Rectangle(hdc, x - r1, y - r1, x + r1, y + r1);
+			SelectObject(hdc, oldpen);
 		}
-		
+		if (select2 == true)
+		{
+			Rectangle(hdc, x - r1, 3 * y - r1, x + r1, 3 * y + r1);
+			SelectObject(hdc, oldpen);
+		}
+		if (select3 == true)
+		{
+			Rectangle(hdc, 3 * x - r1, y - r1, 3 * x + r1, y + r1);
+			SelectObject(hdc, oldpen);
+		}
+		if (select4 == true)
+		{
+			Rectangle(hdc, 3 * x - r1, 3 * y - r1, 3 * x + r1, 3 * y + r1);
+			SelectObject(hdc, oldpen);
+		}
+
 		EndPaint(hWnd, &ps);
 		return 0;
 	}
@@ -183,6 +407,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		DeleteObject(wonbrush);
 		DeleteObject(wonpen);
 		DeleteObject(selectpen);
+		DeleteObject(gongjeon1);
+		DeleteObject(gongjeon2);
+		DeleteObject(gongjeon3);
+		DeleteObject(gongjeon4);
 		PostQuitMessage(0);
 		return 0;
 	}
