9458eaa80eefdf5c22708cd675ccbe00b25647d6
hyeonryeol <hunyeol138@gmail.com>
2026-05-11 16:56:00 +0900

    5-5하는중

--- 실습5/실습5/5-5.cpp
+++ 실습5/실습5/5-5.cpp
@@ -1,27 +1,26 @@
-﻿#pragma comment (lib, "msimg32.lib")
+﻿#pragma comment(lib, "msimg32.lib")
 #include <windows.h>
+#include <atlbase.h>
 #include <atlImage.h>
-
-
-// 윈도우 프로시저 선언
+#include <windowsx.h>
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-HDC hDC, hMem1DC, hMem2DC;
-PAINTSTRUCT ps;
-HBITMAP OldBit[3];
-BITMAP bmp;
-static HBITMAP hBitBackground, hBitCha[4], hBit;
-static RECT rectView;
-static int chaX, chaY, chaCount = 0;
-static int backWidth, backHeight, chaWidth, chaHeight;
 
-// 클래스 이름 / 창 제목
 const wchar_t CLASS_NAME[] = L"MyWindowClass";
 const wchar_t WINDOW_TITLE[] = L"Win32 Basic Template";
 
-// 프로그램 시작점
+PAINTSTRUCT ps;
+static CImage img[5];
+static CImage imgBg;
+static int xPos = 0, yPos = 200;
+static int chaCount = 0;
+static RECT rect;
+
+static CImage imgMouse[3];
+static int mouseX = 400, mouseY = 100;  // ← 마우스 위치
+static int mouseCount = 0;
+bool mouse = false;
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
-    // 1) 윈도우 클래스 등록
     WNDCLASSEXW wc = {};
     wc.cbSize = sizeof(wc);
     wc.style = CS_HREDRAW | CS_VREDRAW;
@@ -32,95 +31,138 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.lpszClassName = CLASS_NAME;
     wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
-
     RegisterClassExW(&wc);
 
-    // 2) 윈도우 생성
-    HWND hWnd = CreateWindowExW(
-        0,
-        CLASS_NAME,
-        WINDOW_TITLE,
+    HWND hWnd = CreateWindowExW(0, CLASS_NAME, WINDOW_TITLE,
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
-        nullptr, nullptr, hInstance, nullptr
-    );
-
+        nullptr, nullptr, hInstance, nullptr);
     if (!hWnd) return 0;
 
-    // 3) 창 표시
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
 
-    // 4) 메시지 루프
     MSG msg = {};
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
+    while (GetMessage(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
     return (int)msg.wParam;
 }
 
-// 윈도우 메시지 처리 함수
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-  
-    static CImage img, imgSprite;
-    static int xPos = 0, yPos = 0;
-    static RECT rect;
-    GetClientRect(hWnd, &rect);
     switch (msg)
     {
     case WM_CREATE:
     {
-        hDC = GetDC(hWnd);
-        GetClientRect(hWnd, &rectView);
-        hBitBackground = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP1)); //--- 배경 이미지 1개
-        hBitCha[0] = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP2)); //--- 애니메이션 이미지 4개
-        hBitCha[1] = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP3));
-        hBitCha[2] = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP4));
-        hBitCha[3] = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP5));
-        img.Load(L"gorani.bmp"); //--- background
-        imgSprite.Load(L"cat.gif"); //--- sprite image
-        SetTimer(hWnd, 1, 16, nullptr);
-        return 0;
-    }
-    case WM_KEYDOWN:
-    {
+        imgBg.Load(L"gorani.bmp");
+        if (imgBg.IsNull())
+            MessageBox(hWnd, L"배경 로드 실패", L"오류", MB_OK);
+
+        for (int i = 0; i < 5; i++) {
+            wchar_t path[32];
+            swprintf(path, 32, L"goranirun%d.png", i + 1);
+            img[i].Load(path);
+            if (img[i].IsNull())
+                MessageBox(hWnd, path, L"로드 실패", MB_OK);
+        }
+        for (int i = 0; i < 3; i++) {
+            wchar_t paths[32];
+            swprintf(paths, 32, L"mouse%d.png", i + 1);
+            imgMouse[i].Load(paths);
+        }
+        GetClientRect(hWnd, &rect);
+        SetTimer(hWnd, 1, 100, nullptr);
         return 0;
     }
 
+    case WM_SIZE:
+    {
+
+        GetClientRect(hWnd, &rect);
+        return 0;
+    }
+    case WM_LBUTTONDOWN:
+    {
+        mouse = true;
+        mouseX = GET_X_LPARAM(lParam);
+        mouseY = GET_Y_LPARAM(lParam);
+        InvalidateRect(hWnd, NULL, FALSE);
+        return 0;
+    }
+    case WM_MOUSEMOVE:
+    {
+        if (wParam & MK_LBUTTON) {  // 왼쪽 버튼 누른 채 이동
+            mouseX = GET_X_LPARAM(lParam);
+            mouseY = GET_Y_LPARAM(lParam);
+            InvalidateRect(hWnd, NULL, FALSE);
+        }
+        return 0;
+    }
+    case WM_LBUTTONUP:
+    {
+        mouse = false;
+    }
     case WM_PAINT:
     {
+        HDC hdc = BeginPaint(hWnd, &ps);
 
-        hDC = BeginPaint(hWnd, &ps);
-        HBITMAP hBitmap;
-        HDC memDC;
-        hBitmap = CreateCompatibleBitmap(hDC, rect.right, rect.bottom);
-        memDC = CreateCompatibleDC(hDC);
-        (HBITMAP)SelectObject(memDC, hBitmap);
-        int w = img.GetWidth();
-        int h = img.GetHeight();
-        img.Draw(memDC, 0, 0, rect.right, rect.bottom, 0, 0, w, h); //--- 메모리 DC에 배경 그리기
-        imgSprite.Draw(memDC, xPos, yPos, 250, 200, 0, 0, 500, 397); //--- 메모리 DC에 스프라이트 그리기
-        BitBlt(hDC, 0, 0, rect.right, rect.bottom, memDC, 0, 0, SRCCOPY); //--- 메모리 DC의 그림을 화면 DC에 복사하기
+        HDC     memDC = CreateCompatibleDC(hdc);
+        HBITMAP hBitmap = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
+        HBITMAP oldBmp = (HBITMAP)SelectObject(memDC, hBitmap);
+
+        FillRect(memDC, &rect, (HBRUSH)GetStockObject(WHITE_BRUSH));
+
+        // 배경
+        if (!imgBg.IsNull())
+            imgBg.Draw(memDC, 0, 0, rect.right, rect.bottom,
+                0, 0, imgBg.GetWidth(), imgBg.GetHeight());
+
+        // 스프라이트
+        if (!img[chaCount].IsNull()) {
+            HDC imgDC = img[chaCount].GetDC();
+            TransparentBlt(memDC, xPos, yPos, 100, 100,
+                imgDC, 0, 0,
+                img[chaCount].GetWidth(),
+                img[chaCount].GetHeight(),
+                RGB(255, 255, 255));
+            img[chaCount].ReleaseDC();
+        }
+        if (mouse == true)
+        {
+
+        if (!imgMouse[mouseCount].IsNull()) {
+            imgMouse[mouseCount].Draw(memDC, mouseX -50, mouseY-50, 100, 100,
+                0, 0,
+                imgMouse[mouseCount].GetWidth(),
+                imgMouse[mouseCount].GetHeight());
+        }
+        }
+        BitBlt(hdc, 0, 0, rect.right, rect.bottom, memDC, 0, 0, SRCCOPY);
+
+        SelectObject(memDC, oldBmp);
         DeleteObject(hBitmap);
         DeleteDC(memDC);
         EndPaint(hWnd, &ps);
         return 0;
     }
-    case WM_TIMER:
-    {
 
+    case WM_TIMER:
         xPos += 5;
-        yPos += 5;
-        InvalidateRect(hWnd, NULL, false);
-        break;
-    }
+        if (xPos > rect.right) xPos = 0;
+        chaCount = (chaCount + 1) % 5;
+        InvalidateRect(hWnd, NULL, FALSE);
+        return 0;
+
+    case WM_KEYDOWN:
+        if (wParam == 'Q') PostQuitMessage(0);
+        return 0;
 
     case WM_DESTROY:
         KillTimer(hWnd, 1);
+        for (int i = 0; i < 5; i++) img[i].Destroy();
+        imgBg.Destroy();
         PostQuitMessage(0);
         return 0;
     }
