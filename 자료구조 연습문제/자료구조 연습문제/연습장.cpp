55823ad6cf4be4e4db6cd6b81d3a3554937894f7
hyeonryeol <hunyeol138@gmail.com>
2026-06-07 23:47:20 +0900

    .

--- 자료구조 연습문제/자료구조 연습문제/연습장.cpp
+++ 자료구조 연습문제/자료구조 연습문제/연습장.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
-#include <fstream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -23,69 +23,159 @@ public:
 	SNode* head;
 	int size;
 
-
 	SinglyLinkedList() : head(nullptr), size(0) {}
 
-	void Insert(Champion c)
+	void Insert_SL(Champion c)
 	{
 		SNode* newNode = new SNode(c);
 		if (!head)
 		{
 			head = newNode;
 		}
-		else {
+		else
+		{
 			SNode* cur = head;
 			while (cur->next) cur = cur->next;
 			cur->next = newNode;
 		}
 		size++;
 	}
-
-	SNode* merge_SL(SNode* a, SNode* b)
+	void Insertsorted(Champion c)
 	{
-		if (!a)return b;
-		if (!b) return a;
-		if (a->data.hp >= b->data.hp)
+		SNode* newNode = new SNode(c);
+		if (!head || head->data.hp >= c.hp)
 		{
-			a->next = merge_SL(a->next, b);
-			return a;
+			newNode->next = head;
+			head = newNode;
 		}
 		else {
-			b->next = merge_SL(a, b->next);
-			return b;
+			SNode* cur = head;
+			while (cur->next && cur->next->data.hp < c.hp)
+				cur = cur->next;
+			newNode->next = cur->next;
+			cur->next = newNode;
 		}
+		size++;
 	}
-
-	SNode* mergeSort_SL(SNode* node) {
-		if (!node || !node->next) return node;
-		SNode* slow = node;
-		SNode* fast = node->next;
-		while (fast && fast->next)
-		{
-			slow = slow->next;
-			fast = fast->next->next;
-		}
-		SNode* mid = slow->next;
-		slow->next = nullptr;
-
-		SNode* left = mergeSort_SL(node);
-		SNode* right = mergeSort_SL(mid);
-		return merge_SL(left, right);
-	}
-
-	void SortByHp() {
-		head = mergeSort_SL(head);
-	}
-
-
-	~SinglyLinkedList() {
+	void PrintAll()
+	{
 		SNode* cur = head;
-		while (cur) {
-			SNode* next = cur->next;
-			delete cur;
-			cur = next;
+		while (cur)
+		{
+			cout << cur->data.position << " "
+				<< cur->data.name << " "
+				<< cur->data.hp << " "
+				<< cur->data.attack << " "
+				<< cur->data.defense << endl;
+			cur = cur->next;
 		}
+	}
+	void Search_SL(string champ)
+	{
+		SNode* cur = head;
+
+		while (cur)
+		{
+			if (champ == cur->data.name)
+			{
+				cout << cur->data.position << " "
+					<< cur->data.name << " "
+					<< cur->data.hp << " "
+					<< cur->data.attack << " "
+					<< cur->data.defense << endl;
+
+			}
+			cur = cur->next;
+		}
+	}
+	void Delete(string champ)
+	{
+
+		if (!head)
+		{
+			cout << "리스트가 비었음" << endl;
+			return;
+		}
+		else if (champ == head->data.name)
+		{
+			SNode* del = head;
+			head = head->next;
+			delete del;
+			size--;
+			return;
+		}
+		SNode* cur = head;
+		while (cur->next && cur->next->data.name != champ)
+			cur = cur->next;
+		if (!cur->next)
+		{
+			cout << "없음" << endl;
+			return;
+		}
+		SNode* del = cur->next;
+		cur->next = del->next;
+		delete del;
+		size--;
+
 	}
 };
 
-Loadfile
+void Loadfile(const string& filename, SinglyLinkedList& sl)
+{
+	ifstream fin(filename);
+	if (!fin.is_open()) {
+		cout << "파일 오류" << endl;
+		return;
+	}
+	Champion c;
+	while (fin >> c.position >> c.name >> c.hp >> c.attack >> c.defense)
+	{
+		sl.Insert_SL(c);
+	}
+	fin.close();
+
+}
+
+int main()
+{
+	SinglyLinkedList sl;
+	Loadfile("test.txt", sl);
+	while (1)
+	{
+		string cmd;
+		cin >> cmd;
+		if (cmd == "printall")
+		{
+			sl.PrintAll();
+		}
+		else if (cmd == "insert")
+		{
+			Champion c;
+			cout << "포지션을 입력하세요." << endl;
+			cin >> c.position;
+			cout << "이름을 입력" << endl;
+			cin >> c.name;
+			cout << "hp" << endl;
+			cin >> c.hp;
+			cout << "attack" << endl;
+			cin >> c.attack;
+			cout << "defense" << endl;
+			cin >> c.defense;
+			sl.Insertsorted(c);
+			cout << "입력완료" << endl;
+		}
+		else if (cmd == "Search")
+		{
+			cout << "찾을 이름" << endl;
+			string champ;
+			cin >> champ;
+			sl.Search_SL(champ);
+		}
+		else if (cmd == "delete")
+		{
+			string name;
+			cin >> name;
+			sl.Delete(name);
+		}
+	}
+}
